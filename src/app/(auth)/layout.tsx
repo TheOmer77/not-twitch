740a0812c104403dec349647d5b8fc3ca0386948
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-17 16:57:57 +0200

    Add Logo to auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import { type PropsWithChildren } from 'react';
+import Logo from '@/components/Logo';
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className='flex h-full w-full flex-col items-center justify-center bg-primary'>
+      <Logo className='text-9xl text-primary-foreground' />
       {children}
     </div>
   );
