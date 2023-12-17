bea48ddeb6dda3d54c7362e742a22a2f503f258c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-17 16:57:46 +0200

    Add Logo to Home nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Logo from '@/components/Logo';
 import { UserButton } from '@clerk/nextjs';
 
 const Home = () => {
   return (
     <nav className='flex h-16 flex-row items-center justify-between gap-4 bg-background px-4 shadow'>
-      <h1 className='text-xl font-bold tracking-tight'>Home</h1>
+      <h1 className='flex flex-row items-center gap-4 text-xl font-bold tracking-tight'>
+        <Logo className='text-5xl text-primary' />
+        Home
+      </h1>
       <UserButton afterSignOutUrl='/' />
     </nav>
   );
