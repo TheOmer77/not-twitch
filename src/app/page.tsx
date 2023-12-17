7d75e8fa214eb1c95d6cd644748b0eb22aa2e680
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-17 14:54:27 +0200

    Add better styles & UserButton to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
 const Home = () => {
-  return <p>If you can see this, you are probably authenticated.</p>;
+  return (
+    <div className='align-center flex flex-row justify-between gap-4 bg-background p-4 shadow'>
+      <h1 className='text-xl font-bold tracking-tight'>Home</h1>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
 export default Home;
