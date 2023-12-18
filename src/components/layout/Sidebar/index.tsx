af3af8554bacaff20a93173b98caab257be0b7d7
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-18 16:21:32 +0200

    Always collapse Sidebar on mobile

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useSidebar } from '@/store/useSidebar';
+import { useMediaQuery } from '@/hooks';
 import { cn } from '@/lib/utils';
 import { CollapseToggle } from './CollapseToggle';
 
 export const Sidebar = () => {
+  const matchesLg = useMediaQuery('(min-width: 1024px)');
   const collapsed = useSidebar(state => state.collapsed);
 
   return (
@@ -12,7 +14,7 @@ export const Sidebar = () => {
       className={cn(
         `fixed start-0 z-20 flex h-full w-80 flex-col items-center border-e
 bg-background p-2 shadow`,
-        collapsed && 'w-20'
+        (collapsed || !matchesLg) && 'w-20'
       )}
     >
       <CollapseToggle />
