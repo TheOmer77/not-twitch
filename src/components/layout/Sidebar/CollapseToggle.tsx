9fa6542b37347b00362d721e6aafbe2f28f0dd71
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-18 15:49:38 +0200

    Add toggle button to collapse/expand sidebar

---
+++ src/components/layout/Sidebar/CollapseToggle.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { ArrowLeftFromLineIcon, ArrowRightFromLineIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Tooltip } from '@/components/ui/tooltip';
+import { useSidebar } from '@/store/useSidebar';
+import { cn } from '@/lib/utils';
+
+export const CollapseToggle = () => {
+  const { collapsed, setCollapsed } = useSidebar();
+
+  const Icon = collapsed ? ArrowRightFromLineIcon : ArrowLeftFromLineIcon;
+
+  return (
+    <Tooltip label={collapsed ? 'Expand' : 'Collapse'} side='right'>
+      <Button
+        variant='ghost'
+        size='icon'
+        onClick={() => setCollapsed(!collapsed)}
+        className={cn('hidden lg:flex', !collapsed && 'absolute end-2 top-2')}
+      >
+        <Icon className='h-4 w-4' />
+      </Button>
+    </Tooltip>
+  );
+};
--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -1,10 +1,22 @@
+'use client';
+
+import { useSidebar } from '@/store/useSidebar';
+import { cn } from '@/lib/utils';
+import { CollapseToggle } from './CollapseToggle';
+
 export const Sidebar = () => {
+  const collapsed = useSidebar(state => state.collapsed);
+
   return (
     <aside
-      className='fixed start-0 z-20 flex h-full w-80 flex-col
-border-e bg-background p-2 shadow'
+      className={cn(
+        `fixed start-0 z-20 flex h-full w-80 flex-col items-center border-e
+bg-background p-2 shadow`,
+        collapsed && 'w-20'
+      )}
     >
-      Sidebar TBD
+      <CollapseToggle />
+      {!collapsed && <>Sidebar TBD</>}
     </aside>
   );
 };
