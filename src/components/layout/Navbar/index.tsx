8afb7b055f34327b20b6c0fe7616335999309936
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-18 14:53:57 +0200

    Tweak & format Navbar classnames

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -6,10 +6,16 @@ import { Actions } from './Actions';
 
 export const Navbar = () => (
   <>
-    <nav className='fixed top-0 flex h-16 w-full flex-row items-center justify-between gap-4 bg-background px-2 shadow md:px-4'>
+    <nav
+      className='fixed top-0 flex h-16 w-full flex-row items-center
+justify-between gap-4 border-b bg-background px-2 shadow md:px-4'
+    >
       <div className='flex flex-row items-center gap-4'>
         <Link href='/'>
-          <Logo className='text-5xl text-primary transition-[opacity,filter] duration-75 hover:brightness-125 active:opacity-70 active:duration-0' />
+          <Logo
+            className='text-5xl text-primary transition-[opacity,filter]
+duration-75 hover:brightness-125 active:opacity-70 active:duration-0'
+          />
         </Link>
         <h1 className='hidden text-xl font-bold tracking-tight md:inline'>
           Home
