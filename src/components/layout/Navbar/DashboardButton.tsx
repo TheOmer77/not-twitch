3be48a94692c07aab0e354d7bec175e68549ce8c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-18 15:44:30 +0200

    Simplify Tooltip component usage

--- src/components/layout/Navbar/DashboardButton.tsx
+++ src/components/layout/Navbar/DashboardButton.tsx
@@ -4,26 +4,14 @@ import Link from 'next/link';
 import { ClapperboardIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
+import { Tooltip } from '@/components/ui/tooltip';
 
 export const DashboardButton = ({ username }: { username: string }) => (
-  <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button variant='secondary' size='icon' asChild>
-          <Link href={`/user/${username}`}>
-            <ClapperboardIcon className='h-4 w-4' />
-          </Link>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>
-        <p>Dashboard</p>
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
+  <Tooltip title='Dashboard'>
+    <Button variant='secondary' size='icon' asChild>
+      <Link href={`/user/${username}`}>
+        <ClapperboardIcon className='h-4 w-4' />
+      </Link>
+    </Button>
+  </Tooltip>
 );
--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,33 +1,44 @@
 'use client';
 
+import { forwardRef, type ElementRef, type ReactNode } from 'react';
 import {
-  forwardRef,
-  type ComponentPropsWithoutRef,
-  type ElementRef,
-} from 'react';
-import {
-  Content,
-  Tooltip,
+  Tooltip as TooltipRoot,
+  TooltipContent,
   TooltipProvider,
   TooltipTrigger,
+  type TooltipContentProps,
 } from '@radix-ui/react-tooltip';
 
 import { cn } from '@/lib/utils';
 
-const TooltipContent = forwardRef<
-  ElementRef<typeof Content>,
-  ComponentPropsWithoutRef<typeof Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-  <Content
-    ref={ref}
-    sideOffset={sideOffset}
-    className={cn(
-      'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-      className
-    )}
-    {...props}
-  />
-));
-TooltipContent.displayName = Content.displayName;
+export type TooltipProps = Omit<TooltipContentProps, 'title'> & {
+  title: ReactNode;
+};
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+export const Tooltip = forwardRef<
+  ElementRef<typeof TooltipContent>,
+  TooltipProps
+>(({ title, sideOffset = 4, className, children, ...props }, ref) => (
+  <TooltipProvider>
+    <TooltipRoot>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipContent
+        {...props}
+        ref={ref}
+        sideOffset={sideOffset}
+        className={cn(
+          `z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5
+text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95
+data-[state=closed]:animate-out data-[state=closed]:fade-out-0
+data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2
+data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2
+data-[side=top]:slide-in-from-bottom-2`,
+          className
+        )}
+      >
+        {title}
+      </TooltipContent>
+    </TooltipRoot>
+  </TooltipProvider>
+));
+Tooltip.displayName = 'Tooltip';
