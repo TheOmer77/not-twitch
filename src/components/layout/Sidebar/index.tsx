8b76d97161cafee75e26f86563f313a83dbd7890
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-18 14:53:19 +0200

    Add SIdebar component

---
+++ src/components/layout/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+export const Sidebar = () => {
+  return (
+    <aside
+      className='fixed start-0 z-20 flex h-full w-80 flex-col
+border-e bg-background p-2 shadow'
+    >
+      Sidebar TBD
+    </aside>
+  );
+};
