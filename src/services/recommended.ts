90aa85c4abfd57733a9fcfe217027f4dd9a323db
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-19 17:41:44 +0200

    Filter current user from recommended

--- src/services/recommended.ts
+++ src/services/recommended.ts
@@ -1,6 +1,17 @@
 import { db } from '@/lib/db';
+import { getSelf } from './auth';
 
 export const getRecommended = async () => {
-  const users = await db.user.findMany({ orderBy: { createdAt: 'desc' } });
+  let currentUserId;
+  try {
+    currentUserId = (await getSelf()).id;
+  } catch (err) {
+    currentUserId = null;
+  }
+
+  const users = await db.user.findMany({
+    orderBy: { createdAt: 'desc' },
+    ...(currentUserId ? { where: { NOT: { id: currentUserId } } } : {}),
+  });
   return users;
 };
