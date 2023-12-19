14b266c4a512d2f788c4234b275bd6a237c68e78
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-19 18:45:50 +0200

    Improve user page

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -1,15 +1,33 @@
-import { db } from '@/lib/db';
 import { notFound } from 'next/navigation';
+
+import { UserAvatar } from '@/components/layout/Sidebar/UserAvatar';
+import { getUserByUsername } from '@/services/users';
 
 type UserPageProps = {
   params: { username: string };
 };
 
 const UserPage = async ({ params: { username } }: UserPageProps) => {
-  const user = await db.user.findUnique({ where: { username } });
+  const user = await getUserByUsername(username);
   if (!user) notFound();
 
-  return <div>{username}&apos;s user page</div>;
+  return (
+    <div>
+      <div className='mb-2 flex flex-row items-center gap-4'>
+        <UserAvatar size='lg' username={username} imageUrl={user.imageUrl} />
+        <h1 className='text-3xl font-bold tracking-tight'>{username}</h1>
+      </div>
+      {user.bio && <p className='text-xl'>{user.bio}</p>}
+      <p className='text-sm text-muted-foreground'>
+        Joined{' '}
+        {new Date(user.createdAt).toLocaleDateString('en-US', {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric',
+        })}
+      </p>
+    </div>
+  );
 };
 
 export default UserPage;
