1d4864a94885d48eb692f9431122118701b35844
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-19 13:34:31 +0200

    Add auth & recommended services

---
+++ src/services/auth.ts
@@ -0,0 +1,17 @@
+import { currentUser } from '@clerk/nextjs';
+import { db } from '../lib/db';
+
+export const getSelf = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser || !clerkUser.username) throw new Error('Unauthorized');
+
+  const dbUser = await db.user.findUnique({
+    where: { externalUserId: clerkUser.id },
+  });
+  if (!dbUser)
+    throw new Error(
+      `User ${clerkUser.username} (external ID: ${clerkUser.id}) exists but isn't found in the DB. This shouldn't happen!`
+    );
+
+  return dbUser;
+};
