02fd3d1332dfdf84f2a068aa16d995798762b9f7
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-19 18:45:06 +0200

    Allow additional props in UserAvatar

--- src/components/layout/Sidebar/UserAvatar.tsx
+++ src/components/layout/Sidebar/UserAvatar.tsx
@@ -1,16 +1,27 @@
 import type { UserItemProps } from './UserItem';
-import { Avatar } from '@/components/ui/avatar';
+import { Avatar, type AvatarProps } from '@/components/ui/avatar';
 import { cn } from '@/lib/utils';
 
-export const UserAvatar = ({ imageUrl, username, isLive }: UserItemProps) => (
+export type UserAvatarProps = UserItemProps &
+  Omit<AvatarProps, 'src' | 'alt' | 'fallback' | 'children'>;
+
+export const UserAvatar = ({
+  imageUrl,
+  username,
+  isLive,
+  className,
+  ...props
+}: UserAvatarProps) => (
   <Avatar
+    {...props}
     src={imageUrl}
     alt={username}
     fallback={username[0]}
     className={cn(
       isLive &&
         `ring-2 ring-red-600 ring-offset-2 ring-offset-white
-dark:ring-red-400 dark:ring-offset-slate-950`
+dark:ring-red-400 dark:ring-offset-slate-950`,
+      className
     )}
   />
 );
