9a2a1cea8a0db0a190083d20108223864af827eb
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-19 15:01:58 +0200

    Add AvatarSkeleton component

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,4 +1,7 @@
 import type { ComponentPropsWithRef } from 'react';
+import type { VariantProps } from 'class-variance-authority';
+
+import { avatarVariants } from './avatar';
 import { cn } from '@/lib/utils';
 
 export const Skeleton = ({
@@ -7,6 +10,20 @@ export const Skeleton = ({
 }: ComponentPropsWithRef<'div'>) => (
   <div
     className={cn('animate-pulse rounded-md bg-muted', className)}
+    {...props}
+  />
+);
+
+export type AvatarSkeletonProps = ComponentPropsWithRef<'div'> &
+  VariantProps<typeof avatarVariants>;
+
+export const AvatarSkeleton = ({
+  size,
+  className,
+  ...props
+}: AvatarSkeletonProps) => (
+  <Skeleton
+    className={cn(avatarVariants({ size, className }), className)}
     {...props}
   />
 );
