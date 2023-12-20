bf7d932af6c9464b083db692b9f2ae20beaa30f7
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-20 16:41:26 +0200

    Add block actions

---
+++ src/actions/block.ts
@@ -0,0 +1,20 @@
+'use server';
+
+import { blockUser, unblockUser } from '@/services/block';
+import { revalidatePath } from 'next/cache';
+
+export const onBlock = async (userId: string) => {
+  // TODO: Adapt to disconnect from livestream
+  // TODO: Allow ability to kick the user
+  const blockedUser = await blockUser(userId);
+  revalidatePath('/');
+  if (blockedUser) revalidatePath(`/${blockedUser.blocked.username}`);
+  return blockedUser;
+};
+
+export const onUnblock = async (userId: string) => {
+  const unblockedUser = await unblockUser(userId);
+  revalidatePath('/');
+  if (unblockedUser) revalidatePath(`/${unblockedUser.blocked.username}`);
+  return unblockedUser;
+};
