dc6d02e3f620dfac1ec154115298c1a9b6ef7e8b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-21 18:30:57 +0200

    Create a stream on user creation webhook

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -46,6 +46,7 @@ export const POST = async (req: Request) => {
           externalUserId: payload.data.id,
           username: payload.data.username,
           imageUrl: payload.data.image_url,
+          stream: { create: { name: `${payload.data.username}'s stream` } },
         },
       });
       break;
