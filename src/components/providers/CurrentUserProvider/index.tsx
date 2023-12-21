5f791cf04081ada6e5ef80d1f4b5586dfcf8bf67
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-21 18:16:57 +0200

    Fix broken CurrentUserProvider when logged out

--- src/components/providers/CurrentUserProvider/index.tsx
+++ src/components/providers/CurrentUserProvider/index.tsx
@@ -4,7 +4,12 @@ import ClientCurrentUserProvider from './clientProvider';
 import { getSelf } from '@/services/auth';
 
 export const CurrentUserProvider = async ({ children }: PropsWithChildren) => {
-  const currentUser = await getSelf();
+  let currentUser;
+  try {
+    currentUser = await getSelf();
+  } catch {
+    currentUser = null;
+  }
   return (
     <ClientCurrentUserProvider value={currentUser}>
       {children}
