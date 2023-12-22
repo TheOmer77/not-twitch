1d3f1c62520036d1cd911138f70eb95efe7b448e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-22 23:32:41 +0200

    Add `asChild` prop to Card

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,17 +1,24 @@
 import { forwardRef, type ComponentPropsWithoutRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 import { cn } from '@/lib/utils';
 
-export const Card = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        'rounded-lg bg-card text-card-foreground shadow-sm',
-        className
-      )}
-      {...props}
-    />
-  )
+export type CardProps = ComponentPropsWithoutRef<'div'> & { asChild?: boolean };
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ asChild, className, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'div';
+
+    return (
+      <Comp
+        ref={ref}
+        className={cn(
+          'rounded-lg bg-card text-card-foreground shadow-sm',
+          className
+        )}
+        {...props}
+      />
+    );
+  }
 );
 Card.displayName = 'Card';
