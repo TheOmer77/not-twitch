051fc96208298cd6b6d20d7f2640c6963111e283
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-22 16:25:14 +0200

    Add UI for dashboard keys page

--- src/app/dashboard/keys/page.tsx
+++ src/app/dashboard/keys/page.tsx
@@ -1,5 +1,44 @@
-const DashboardKeysPage = () => {
-  return <div>Dashboard keys</div>;
+import { InputSettingsItem } from '@/components/layout';
+import { Button } from '@/components/ui/Button';
+import { Card } from '@/components/ui/Card';
+import { getStreamByUserId } from '@/queries/stream';
+import { getSelf } from '@/services/auth';
+
+const DashboardKeysPage = async () => {
+  const currentUser = await getSelf(),
+    stream = await getStreamByUserId(currentUser.id);
+  if (!stream) throw new Error("You don't have a stream.");
+
+  return (
+    <>
+      <h1 className='mb-4 flex flex-row justify-between text-4xl font-bold tracking-tight'>
+        Keys
+        <Button variant='primary'>Generate</Button>
+      </h1>
+      <Card>
+        <ul className='flex w-full flex-col gap-px'>
+          <InputSettingsItem
+            field='serverUrl'
+            label='Server URL'
+            placeholder='Server URL'
+            value={stream.serverUrl || ''}
+            disabled
+            withCopyButton
+          />
+          <InputSettingsItem
+            field='streamKey'
+            label='Stream key'
+            placeholder='Stream key'
+            value={stream.streamKey || ''}
+            type='password'
+            disabled
+            withCopyButton
+            secret
+          />
+        </ul>
+      </Card>
+    </>
+  );
 };
 
 export default DashboardKeysPage;
