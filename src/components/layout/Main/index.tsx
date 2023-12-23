4e1d640571a423958bdf2a885e5f45fd71f55d1d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-23 22:40:51 +0200

    Add `full` prop to Main, increasing its width

--- src/components/layout/Main/index.tsx
+++ src/components/layout/Main/index.tsx
@@ -5,14 +5,18 @@ import { type PropsWithChildren } from 'react';
 import { cn } from '@/lib/utils';
 import { useSidebar } from '@/store/useSidebar';
 
-export const Main = ({ children }: PropsWithChildren) => {
+export type MainProps = PropsWithChildren<{ full?: boolean }>;
+
+export const Main = ({ full, children }: MainProps) => {
   const collapsed = useSidebar(state => state.collapsed);
 
   return (
     <main
       className={cn(
-        'mx-auto max-w-6xl p-4 ps-24 lg:ps-[21rem]',
-        collapsed && 'lg:max-w-6xl lg:ps-4'
+        'p-4 ps-24 lg:ps-[21rem]',
+        !full && 'mx-auto max-w-6xl',
+        collapsed && 'lg:ps-4',
+        collapsed && !full && 'lg:max-w-6xl'
       )}
     >
       {children}
