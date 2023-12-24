4243ee4a22b59cfcb2adf3a802bd958ed01c2c89
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-24 20:02:03 +0200

    Implement stream volume control & muting
    Note that currently video is muted by default, due to a Chrome policy.

--- src/components/layout/StreamPlayer/StreamLiveVideo.tsx
+++ src/components/layout/StreamPlayer/StreamLiveVideo.tsx
@@ -1,26 +1,53 @@
 'use client';
 
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Track, type Participant } from 'livekit-client';
 import { useTracks } from '@livekit/components-react';
 import { useEventListener } from 'usehooks-ts';
 
 import { StreamFullscreenControl } from './StreamFullscreenControl';
+import { StreamVolumeControl } from './StreamVolumeControl';
 
 export type StreamLiveVideoProps = {
   participant: Participant;
 };
 
 export const StreamLiveVideo = ({ participant }: StreamLiveVideoProps) => {
-  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [volume, setVolume] = useState(100),
+    [muted, setMuted] = useState(true),
+    [isFullscreen, setIsFullscreen] = useState(false);
 
   const wrapperRef = useRef<HTMLDivElement>(null),
     videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVolumeChange = useCallback((value: number) => {
+    setVolume(value);
+    if (value > 0) setMuted(false);
+
+    if (!videoRef.current) return;
+    videoRef.current.muted = value < 1;
+    videoRef.current.volume = value * 0.01;
+  }, []);
+
+  const handleMutedChange = useCallback(
+    (muted: boolean) => setMuted(muted),
+    []
+  );
 
   const handleFullscreenChange = useCallback((value: boolean) => {
     if (!value) document.exitFullscreen();
     else if (wrapperRef.current) wrapperRef.current.requestFullscreen();
   }, []);
+
+  useEffect(() => {
+    handleMutedChange(true);
+  }, [handleMutedChange]);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    videoRef.current.muted = muted;
+    videoRef.current.volume = muted ? 0 : volume * 0.01;
+  }, [muted, volume]);
 
   useEventListener(
     'fullscreenchange',
@@ -43,6 +70,12 @@ export const StreamLiveVideo = ({ participant }: StreamLiveVideoProps) => {
 justify-between bg-gradient-to-t from-neutral-900 px-2 opacity-0
 transition-opacity group-hover:opacity-100'
       >
+        <StreamVolumeControl
+          value={volume}
+          onValueChange={handleVolumeChange}
+          muted={muted}
+          onMutedChange={handleMutedChange}
+        />
         <StreamFullscreenControl
           isFullscreen={isFullscreen}
           onFullscreenChange={handleFullscreenChange}
---
+++ src/components/layout/StreamPlayer/StreamVolumeControl.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import {
+  Volume1Icon,
+  Volume2Icon,
+  VolumeIcon,
+  VolumeXIcon,
+} from 'lucide-react';
+
+import { Tooltip } from '@/components/ui/Tooltip';
+import { Slider } from '@/components/ui/Slider';
+import { Button } from '@/components/ui/Button';
+import { useCallback } from 'react';
+
+export type StreamVolumeControlProps = {
+  value?: number;
+  onValueChange?: (value: number) => void;
+  muted?: boolean;
+  onMutedChange?: (muted: boolean) => void;
+};
+
+export const StreamVolumeControl = ({
+  value = 0,
+  onValueChange,
+  muted = false,
+  onMutedChange,
+}: StreamVolumeControlProps) => {
+  const Icon = muted
+    ? VolumeXIcon
+    : value < 1
+      ? VolumeIcon
+      : value < 50
+        ? Volume1Icon
+        : Volume2Icon;
+
+  const handleValueChange = useCallback(
+    (value: number[]) => onValueChange?.(value[0]),
+    [onValueChange]
+  );
+
+  return (
+    <div className='flex items-center gap-2'>
+      <Tooltip label={muted || value < 1 ? 'Unmute' : 'Mute'}>
+        <Button
+          variant='flat'
+          size='icon'
+          onClick={() => onMutedChange?.(!muted)}
+        >
+          <Icon className='h-4 w-4' />
+        </Button>
+      </Tooltip>
+      <Slider
+        value={[muted ? 0 : value]}
+        onValueChange={handleValueChange}
+        max={100}
+        step={1}
+        className='w-32'
+      />
+    </div>
+  );
+};
