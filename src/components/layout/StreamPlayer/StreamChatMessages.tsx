981ca7bc1533cd14489e1467c920ec45610976f6
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-25 21:07:50 +0200

    Improve scrolling behavior of chat
    - Auto scroll to bottom when a new message is sent
    - If manually scrolling up, disable autoscroll behavior and display a
    button to scroll back down
    - Use the `scrollend` event for this, which is not supported in Safari
    yet (`scrollyfills` helps here)

--- src/components/layout/StreamPlayer/StreamChatMessages.tsx
+++ src/components/layout/StreamPlayer/StreamChatMessages.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useChat } from '@livekit/components-react';
+import { useEventListener } from 'usehooks-ts';
+import { ArrowDownIcon } from 'lucide-react';
+import 'scrollyfills';
 
 import { StreamChatMessage } from './StreamChatMessage';
+import { Button } from '@/components/ui/Button';
+import { cn } from '@/lib/utils';
 
 export type StreamChatMessagesProps = {
   isChatEnabled: boolean;
@@ -15,11 +20,34 @@ export const StreamChatMessages = ({
   isChatEnabled,
   isOnline,
 }: StreamChatMessagesProps) => {
+  const listRef = useRef<HTMLUListElement>(null);
+  const [scrolledToBottom, setScrolledToBottom] = useState(true);
   const { chatMessages } = useChat();
   const messages = useMemo(
     () => [...chatMessages].sort((a, b) => a.timestamp - b.timestamp),
     [chatMessages]
   );
+
+  const scrollToBottom = () => {
+    if (!listRef.current) return;
+    const { scrollHeight } = listRef.current;
+    listRef.current.scrollTop = scrollHeight;
+  };
+
+  useEventListener(
+    'scrollend',
+    () => {
+      if (!listRef.current) return;
+      const { scrollTop, scrollHeight, clientHeight } = listRef.current,
+        atBottom = scrollTop + clientHeight >= scrollHeight;
+      if (scrolledToBottom !== atBottom) setScrolledToBottom(atBottom);
+    },
+    listRef
+  );
+
+  useEffect(() => {
+    if (scrolledToBottom) scrollToBottom();
+  }, [messages.length, scrolledToBottom]);
 
   return !isOnline ? (
     <p
@@ -36,14 +64,30 @@ text-muted-foreground'
       This stream&apos;s chat is disabled.
     </p>
   ) : (
-    <div className='grow overflow-auto break-words'>
-      <p className='mb-2 text-sm text-muted-foreground'>Welcome to the chat!</p>
-      {messages.map(message => (
-        <StreamChatMessage
-          key={`${message.from?.name}-${message.timestamp}`}
-          data={message}
-        />
-      ))}
+    <div className='relative flex grow flex-col overflow-auto'>
+      <ul ref={listRef} className='flex-grow overflow-auto break-words'>
+        <li className='mb-2 text-sm text-muted-foreground'>
+          Welcome to the chat!
+        </li>
+        {messages.map(message => (
+          <StreamChatMessage
+            key={`${message.from?.name}-${message.timestamp}`}
+            data={message}
+          />
+        ))}
+      </ul>
+      <Button
+        onClick={scrollToBottom}
+        disabled={scrolledToBottom}
+        className={cn(
+          `absolute bottom-2 self-center transition-[background-color,opacity]
+disabled:opacity-0`,
+          scrolledToBottom && 'pointer-events-none'
+        )}
+      >
+        <ArrowDownIcon className='me-2 h-4 w-4' />
+        See latest messages
+      </Button>
     </div>
   );
 };
