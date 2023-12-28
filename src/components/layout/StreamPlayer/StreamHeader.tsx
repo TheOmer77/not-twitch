9b2607f6eb8c1a64e91c8abaa4ad0a4dcbddd523
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-28 15:26:59 +0200

    Move StreamHeader action to their own component

--- src/components/layout/StreamPlayer/StreamHeader.tsx
+++ src/components/layout/StreamPlayer/StreamHeader.tsx
@@ -1,66 +1,23 @@
 'use client';
 
-import { useCallback, useTransition } from 'react';
-import { useRouter } from 'next/navigation';
 import {
   useParticipants,
   useRemoteParticipant,
 } from '@livekit/components-react';
-import { useAuth } from '@clerk/nextjs';
 import { User2Icon } from 'lucide-react';
 
-import { Button } from '@/components/ui/Button';
-import { UserAvatar } from '@/components/layout/User';
-import { useStream, useToast } from '@/hooks';
-import { Tooltip } from '@/components/ui/Tooltip';
-import { followUser, unfollowUser } from '@/actions/follow';
+import { useStream } from '@/hooks';
+import { StreamHeaderActions } from './StreamHeaderActions';
 
 export type StreamHeaderProps = { name: string; imageUrl: string };
 
 export const StreamHeader = ({ name, imageUrl }: StreamHeaderProps) => {
-  const { userId } = useAuth();
-  const { hostId, hostName, isFollowing, viewerId } = useStream();
+  const { hostId } = useStream();
   const participants = useParticipants(),
     hostParticipant = useRemoteParticipant(hostId);
 
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const { displayToast } = useToast();
-
   const isLive = !!hostParticipant;
-  const isHost = viewerId === `host-${hostId}`;
   const participantCount = participants.length - 1;
-
-  const handleFollowClick = useCallback(() => {
-    if (isHost) return;
-
-    startTransition(async () => {
-      try {
-        const follow = await (isFollowing
-          ? unfollowUser(hostId)
-          : followUser(hostId));
-        displayToast(
-          isFollowing
-            ? `You are no longer following ${follow.followedUser.username}.`
-            : `You are now following ${follow.followedUser.username}!`
-        );
-      } catch (err) {
-        displayToast(
-          isFollowing
-            ? `Couldn't unfollow this user`
-            : `Couldn't follow this user`,
-          {
-            description:
-              err instanceof Error
-                ? err.message
-                : isFollowing
-                  ? 'Something went wrong while trying to unfollow this user.'
-                  : 'Something went wrong while trying to follow this user.',
-          }
-        );
-      }
-    });
-  }, [displayToast, hostId, isFollowing, isHost]);
 
   return (
     <div className='relative px-2 py-4'>
@@ -81,27 +38,7 @@ bg-destructive px-1.5 text-sm font-medium text-destructive-foreground'
           </span>
         )}
       </div>
-      <div className='flex h-10 flex-row items-center gap-2'>
-        <UserAvatar username={hostName} imageUrl={imageUrl} isLive={isLive} />
-        <span className='grow text-sm font-semibold'>{hostName}</span>
-        {!userId ? (
-          <Tooltip label={`To follow ${hostName}, you must log in first.`}>
-            <Button variant='primary' onClick={() => router.push('/login')}>
-              Follow
-            </Button>
-          </Tooltip>
-        ) : (
-          !isHost && (
-            <Button
-              variant={isFollowing ? 'default' : 'primary'}
-              onClick={handleFollowClick}
-              disabled={isPending}
-            >
-              {isFollowing ? 'Unfollow' : 'Follow'}
-            </Button>
-          )
-        )}
-      </div>
+      <StreamHeaderActions imageUrl={imageUrl} />
     </div>
   );
 };
---
+++ src/components/layout/StreamPlayer/StreamHeaderActions.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { useCallback, useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+import { useRemoteParticipant } from '@livekit/components-react';
+import { useAuth } from '@clerk/nextjs';
+
+import { Tooltip } from '@/components/ui/Tooltip';
+import { Button } from '@/components/ui/Button';
+import { UserAvatar } from '@/components/layout/User';
+import { useStream, useToast } from '@/hooks';
+import { followUser, unfollowUser } from '@/actions/follow';
+
+export type StreamHeaderActionsProps = { imageUrl: string };
+
+export const StreamHeaderActions = ({ imageUrl }: StreamHeaderActionsProps) => {
+  const { userId } = useAuth();
+  const { isFollowing, hostName, hostId, viewerId } = useStream();
+  const hostParticipant = useRemoteParticipant(hostId);
+
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { displayToast } = useToast();
+
+  const isLive = !!hostParticipant;
+  const isHost = viewerId === `host-${hostId}`;
+
+  const handleFollowClick = useCallback(() => {
+    if (isHost) return;
+
+    startTransition(async () => {
+      try {
+        const follow = await (isFollowing
+          ? unfollowUser(hostId)
+          : followUser(hostId));
+        displayToast(
+          isFollowing
+            ? `You are no longer following ${follow.followedUser.username}.`
+            : `You are now following ${follow.followedUser.username}!`
+        );
+      } catch (err) {
+        displayToast(
+          isFollowing
+            ? `Couldn't unfollow this user`
+            : `Couldn't follow this user`,
+          {
+            description:
+              err instanceof Error
+                ? err.message
+                : isFollowing
+                  ? 'Something went wrong while trying to unfollow this user.'
+                  : 'Something went wrong while trying to follow this user.',
+          }
+        );
+      }
+    });
+  }, [displayToast, hostId, isFollowing, isHost]);
+
+  return (
+    <div className='flex h-10 flex-row items-center gap-2'>
+      <UserAvatar username={hostName} imageUrl={imageUrl} isLive={isLive} />
+      <span className='grow text-sm font-semibold'>{hostName}</span>
+      {!userId ? (
+        <Tooltip label={`To follow ${hostName}, you must log in first.`}>
+          <Button variant='primary' onClick={() => router.push('/login')}>
+            Follow
+          </Button>
+        </Tooltip>
+      ) : (
+        !isHost && (
+          <Button
+            variant={isFollowing ? 'default' : 'primary'}
+            onClick={handleFollowClick}
+            disabled={isPending}
+          >
+            {isFollowing ? 'Unfollow' : 'Follow'}
+          </Button>
+        )
+      )}
+    </div>
+  );
+};
