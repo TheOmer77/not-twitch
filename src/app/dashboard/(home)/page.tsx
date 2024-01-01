4edd4951f25de7a139d2655fe4e82fa86c0f9855
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-02 00:43:05 +0200

    Redirect logged out user to `/login` from dashboard

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -1,18 +1,24 @@
+import { redirect } from 'next/navigation';
+
 import { StreamPlayer } from '@/components/layout';
 import { getCurrentUser } from '@/queries/auth';
 
 const DashboardHomePage = async () => {
-  const { stream, ...user } = await getCurrentUser({
-    includeStream: true,
-    includeFollowerCount: true,
-  });
-  if (!stream) throw new Error("You don't have a stream.");
+  try {
+    const { stream, ...user } = await getCurrentUser({
+      includeStream: true,
+      includeFollowerCount: true,
+    });
+    if (!stream) throw new Error("You don't have a stream.");
 
-  return (
-    <div className='h-full'>
-      <StreamPlayer user={user} stream={stream} isFollowing={true} />
-    </div>
-  );
+    return (
+      <div className='h-full'>
+        <StreamPlayer user={user} stream={stream} isFollowing={true} />
+      </div>
+    );
+  } catch {
+    redirect('/login');
+  }
 };
 
 export default DashboardHomePage;
