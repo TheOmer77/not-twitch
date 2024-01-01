bc9368bc77679f91618c7fe7574f51ddf7e62298
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-02 00:46:07 +0200

    Add loading state to user page

---
+++ src/app/(browse)/[username]/loading.tsx
@@ -0,0 +1,9 @@
+import { StreamPlayerSkeleton } from '@/components/layout';
+
+const UserPageLoading = () => (
+  <div className='h-full'>
+    <StreamPlayerSkeleton />
+  </div>
+);
+
+export default UserPageLoading;
