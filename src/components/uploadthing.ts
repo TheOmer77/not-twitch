23c5b24dbc3583f2da4b3c12bac627b3a8439b0f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-03 16:07:54 +0200

    Remove unused uploadthing components

--- src/components/uploadthing.ts
+++
@@ -1,6 +0,0 @@
-import { generateComponents } from '@uploadthing/react';
-
-import type { ThumbnailFileRouter } from '@/lib/uploadthing';
-
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<ThumbnailFileRouter>();
