4a0d19db689355292164d5821ac032aa18bc4b30
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-08 14:28:22 +0200

    Add Shadcn label

---
+++ src/components/ui/Label/Label.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react';
+import { Label as LabelRoot } from '@radix-ui/react-label';
+
+import { cn } from '@/lib/utils';
+
+export const Label = forwardRef<
+  ElementRef<typeof LabelRoot>,
+  ComponentPropsWithoutRef<typeof LabelRoot>
+>(({ className, ...props }, ref) => (
+  <LabelRoot
+    ref={ref}
+    className={cn(
+      `text-sm font-medium leading-none peer-disabled:cursor-not-allowed
+peer-disabled:opacity-70`,
+      className
+    )}
+    {...props}
+  />
+));
+Label.displayName = LabelRoot.displayName;
