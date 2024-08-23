a6d7c4d64cf587cb71f7c68c516c38de3610a586
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-23 20:00:22 +0300

    Redirect from auth routes if logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,35 @@
+import { NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/api/webhooks(.*)',
-  '/api/uploadthing',
-  '/:username',
-  '/search',
-]);
+import { SIGN_IN_URL, SIGN_UP_URL } from '@/constants/clerk';
 
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) auth().protect();
-});
+const publicRoutes = [
+    '/',
+    '/api/webhooks(.*)',
+    '/api/uploadthing',
+    '/:username',
+    '/search',
+  ],
+  authRoutes = [SIGN_IN_URL, SIGN_UP_URL];
+const isPublicRoute = createRouteMatcher(publicRoutes),
+  isAuthRoute = createRouteMatcher(authRoutes);
+
+export default clerkMiddleware(
+  (auth, req) => {
+    const { userId, protect } = auth();
+    if (!isPublicRoute(req) && !isAuthRoute(req)) protect();
+    if (isAuthRoute(req) && userId)
+      return NextResponse.redirect(
+        req.nextUrl.searchParams.get('redirect_url') ||
+          process.env.NEXT_PUBLIC_APP_URL!
+      );
+  },
+  { signInUrl: SIGN_IN_URL, signUpUrl: SIGN_UP_URL }
+);
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
