52a868545e8bf24a326ce929bbffca1556176b24
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-23 22:06:13 +0300

    Fix incorrect shades setup in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,64 +13,63 @@ const config: Config = withUt({
   ],
   theme: {
     container: { center: true, padding: '2rem', screens: { '2xl': '1400px' } },
-    colors: {
-      inherit: 'inherit',
-      white: '#fff',
-      black: '#000',
-      transparent: 'transparent',
+    colors: ['primary', 'neutral', 'destructive'].reduce(
+      (obj, colorName) => ({
+        ...obj,
+        [colorName]: [
+          50,
+          ...[...Array(9).keys()].map(key => (key + 1) * 100),
+          950,
+        ].reduce(
+          (shadesObj, shade) => ({
+            ...shadesObj,
+            [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
+          }),
+          obj?.[colorName as keyof typeof obj] || {}
+        ),
+      }),
+      {
+        inherit: 'inherit',
+        white: '#fff',
+        black: '#000',
+        transparent: 'transparent',
 
-      ...['primary', 'neutral', 'destructive'].reduce(
-        (obj, colorName) => ({
-          ...obj,
-          [colorName]: [
-            50,
-            ...[...Array(9).keys()].map(key => (key + 1) * 100),
-            950,
-          ].reduce(
-            (obj, shade) => ({
-              ...obj,
-              [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
-            }),
-            {}
-          ),
-        }),
-        {}
-      ),
+        border: 'rgb(var(--color-border))',
+        input: 'rgb(var(--color-input))',
+        ring: 'rgb(var(--color-ring))',
+        background: 'rgb(var(--color-background))',
+        foreground: 'rgb(var(--color-foreground))',
+        primary: {
+          DEFAULT: 'rgb(var(--color-primary))',
+          foreground: 'rgb(var(--color-primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'rgb(var(--color-secondary))',
+          foreground: 'rgb(var(--color-secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'rgb(var(--color-destructive))',
+          foreground: 'rgb(var(--color-destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'rgb(var(--color-muted))',
+          foreground: 'rgb(var(--color-muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'rgb(var(--color-accent))',
+          foreground: 'rgb(var(--color-accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'rgb(var(--color-popover))',
+          foreground: 'rgb(var(--color-popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'rgb(var(--color-card))',
+          foreground: 'rgb(var(--color-card-foreground))',
+        },
+      }
+    ),
 
-      border: 'rgb(var(--color-border))',
-      input: 'rgb(var(--color-input))',
-      ring: 'rgb(var(--color-ring))',
-      background: 'rgb(var(--color-background))',
-      foreground: 'rgb(var(--color-foreground))',
-      primary: {
-        DEFAULT: 'rgb(var(--color-primary))',
-        foreground: 'rgb(var(--color-primary-foreground))',
-      },
-      secondary: {
-        DEFAULT: 'rgb(var(--color-secondary))',
-        foreground: 'rgb(var(--color-secondary-foreground))',
-      },
-      destructive: {
-        DEFAULT: 'rgb(var(--color-destructive))',
-        foreground: 'rgb(var(--color-destructive-foreground))',
-      },
-      muted: {
-        DEFAULT: 'rgb(var(--color-muted))',
-        foreground: 'rgb(var(--color-muted-foreground))',
-      },
-      accent: {
-        DEFAULT: 'rgb(var(--color-accent))',
-        foreground: 'rgb(var(--color-accent-foreground))',
-      },
-      popover: {
-        DEFAULT: 'rgb(var(--color-popover))',
-        foreground: 'rgb(var(--color-popover-foreground))',
-      },
-      card: {
-        DEFAULT: 'rgb(var(--color-card))',
-        foreground: 'rgb(var(--color-card-foreground))',
-      },
-    },
     extend: {
       fontFamily: { sans: ['var(--font-family)', 'sans-serif'] },
       spacing: { em: '1em' },
