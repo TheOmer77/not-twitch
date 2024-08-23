b6c1b590d849ab37f00ae460e58b1907936fb164
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-23 23:13:14 +0300

    Add Link component

---
+++ src/components/ui/Link/Link.tsx
@@ -0,0 +1,32 @@
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react';
+import NextLink from 'next/link';
+import { Slot } from '@radix-ui/react-slot';
+import { cn } from '@/lib/utils';
+
+export type LinkProps = ComponentPropsWithoutRef<typeof NextLink> & {
+  asChild?: boolean;
+};
+
+export const Link = forwardRef<ElementRef<typeof NextLink>, LinkProps>(
+  ({ href, asChild, className, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : NextLink;
+    return (
+      <Comp
+        {...props}
+        ref={ref}
+        href={href}
+        className={cn(
+          `text-primary rounded-sm font-medium underline-offset-4 hover:underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50`,
+          className
+        )}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
+Link.displayName = 'Link';
